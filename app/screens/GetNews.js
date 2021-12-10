7aaa94631cb52825ed2ca530afb2fdeef8760cbf
malsha11 <96194@fhss.sjp.ac.lk>
2021-12-10 22:30:55 +0530

    Call newsApi for getNews to getNews Screen

--- app/screens/GetNews.js
+++ app/screens/GetNews.js
@@ -1,11 +1,28 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 class GetNews extends Component {
-  state = {};
+  state = {
+      news:[]
+  };
   componentDidMount() {
     this.props.navigation.setOptions({
       title: this.props.route.params.category,
     });
+
+    fetch(
+        `https://newsapi.org/v2/top-headlines?
+        category=${}&country=in&apiKey=${config.API_KEY}`,
+      )
+        .then(res => res.json())
+        .then(response => {
+          this.setState({
+            news: response.articles,
+          });
+          setLoading(false)
+        })
+        .catch(error => {
+          console.log(error);
+        });
   }
   render() {
     return (
