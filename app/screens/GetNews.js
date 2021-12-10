92c698e120b30766996bf09456c42e96c4eca05a
malsha11 <96194@fhss.sjp.ac.lk>
2021-12-10 11:21:12 +0530

    update category Names

--- app/screens/GetNews.js
+++ app/screens/GetNews.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';
-import{ Text, View } from 'react-native';
-class GetNews extends Component{
-    state = { }
-    render() {
-        return ( 
-            <View>
-                <Text>{this.props.route.params.category}</Text>
-            </View>
-        );
-    }
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+class GetNews extends Component {
+  state = {};
+  componentDidMount() {
+    this.props.navigation.setOptions({
+      title: this.props.route.params.category,
+    });
+  }
+  render() {
+    return (
+      <View>
+        <Text>{this.props.route.params.category}</Text>
+      </View>
+    );
+  }
 }
 export default GetNews;
