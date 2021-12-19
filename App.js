9f6c69fdf8d8aac19a1aa5418dd05b92b2ca9054
malsha11 <96194@fhss.sjp.ac.lk>
2021-12-19 20:42:10 +0530

    Add Async Storage

--- App.js
+++ App.js
@@ -7,27 +7,47 @@ import HomeScreen from './app/screens/HomeScreen';
 import GetNews from './app/screens/GetNews';
 import OnboardingScreen from './app/screens/OnboardingScreen';
 import LoginScreen from './app/screens/LoginScreen';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import WebViewComponent from './app/components/WebView';
 
 const AppStack = createStackNavigator();
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <AppStack.Navigator headerMode="none">
-        <AppStack.Screen name="Onboarding" component={OnboardingScreen} />
-        <AppStack.Screen name="Login" component={LoginScreen} />
-        <AppStack.Screen name="Trending" component={HomeScreen} />
-        <AppStack.Screen name="GetNews" component={GetNews} />
-        <AppStack.Screen
-          name="WebView"
-          component={WebViewComponent}
-          options={{headerShown: false}}
-        />
-      </AppStack.Navigator>
-    </NavigationContainer>
-  );
+  const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
+
+  React.useEffect(() => {
+    AsyncStorage.getItem('alreadyLaunched').then(value => {
+      if ((value = null)) {
+        AsyncStorage.setItem('alreadyLaunched', 'true');
+        setIsFirstLaunch(true);
+      } else {
+        setIsFirstLaunch(false);
+      }
+    });
+  }, []);
+
+  if (isFirstLaunch === null) {
+    return null;
+  } else if (isFirstLaunch === true) {
+    return (
+      <NavigationContainer>
+        <AppStack.Navigator headerMode="none">
+          <AppStack.Screen name="Onboarding" component={OnboardingScreen} />
+          <AppStack.Screen name="Login" component={LoginScreen} />
+          <AppStack.Screen name="Trending" component={HomeScreen} />
+          <AppStack.Screen name="GetNews" component={GetNews} />
+          <AppStack.Screen
+            name="WebView"
+            component={WebViewComponent}
+            options={{headerShown: false}}
+          />
+        </AppStack.Navigator>
+      </NavigationContainer>
+    );
+  } else {
+    return <LoginScreen />;
+  }
 };
 
 export default App;
