1a71f7e158ba1f9f437eca3037fa522f782ee7c9
malsha11 <96194@fhss.sjp.ac.lk>
2021-12-20 16:59:52 +0530

    create SignUp Screen

--- App.js
+++ App.js
@@ -1,55 +1,8 @@
-/* eslint-disable react-native/no-inline-styles */
-import * as React from 'react';
-import {useEffect, useState} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-
-import HomeScreen from './app/screens/HomeScreen';
-import GetNews from './app/screens/GetNews';
-import OnboardingScreen from './app/screens/OnboardingScreen';
-import LoginScreen from './app/screens/LoginScreen';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-import WebViewComponent from './app/components/WebView';
-
-
-const AppStack = createStackNavigator();
+import React from 'react';
+import Providers from './app/navigation';
 
 const App = () => {
-  const [isFirstLaunch, setIsFirstLaunch] = useState(null);
-
-  useEffect(() => {
-    AsyncStorage.getItem('alreadyLanuch').then(value => {
-      if (value == null) {
-        AsyncStorage.setItem('alreadyLunched', 'true');
-        setIsFirstLaunch(true);
-      } else {
-        setIsFirstLaunch(false);
-      }
-    });
-  }, []);
-
-  if (isFirstLaunch === null) {
-    return null;
-  } else if (isFirstLaunch === true) {
-    return (
-      <NavigationContainer>
-        <AppStack.Navigator headerMode="none">
-          <AppStack.Screen name="Onboarding" component={OnboardingScreen} />
-          <AppStack.Screen name="Login" component={LoginScreen} />
-          <AppStack.Screen name="Trending" component={HomeScreen} />
-          <AppStack.Screen name="GetNews" component={GetNews} />
-          <AppStack.Screen
-            name="WebView"
-            component={WebViewComponent}
-            options={{headerShown: false}}
-          />
-        </AppStack.Navigator>
-      </NavigationContainer>
-    );
-  } else {
-    return <LoginScreen />;
-  }
+  return <Providers />;
 };
 
 export default App;
--- app/navigation/Routes.js
+++ app/navigation/Routes.js
@@ -1,4 +1,4 @@
-import {useEffect, useState, useContext} from 'react';
+import React, {useEffect, useState, useContext} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AuthStack from './AluthStack';
 
