dec79c448484bced545de5ee51d7893af7597172
malsha11 <96194@fhss.sjp.ac.lk>
2021-12-20 15:52:26 +0530

    AulthStack for navigation

---
+++ app/navigation/AluthStack.js
@@ -0,0 +1,67 @@
+/* eslint-disable react-native/no-inline-styles */
+import * as React from 'react';
+import {useEffect, useState} from 'react';
+import {View} from 'react-native';
+//import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import OnboardingScreen from '../screens/OnboardingScreen';
+import SignupScreen from '../screens/SignupScreen';
+import LoginScreen from '../screens/LoginScreen';
+import HomeScreen from '../screens/HomeScreen';
+import GetNews from '../screens/GetNews';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import WebViewComponent from '../components/WebView';
+
+const Stack = createStackNavigator();
+
+const AuthStack = () => {
+  const [isFirstLaunch, setIsFirstLaunch] = useState(null);
+  let routeName;
+
+  useEffect(() => {
+    AsyncStorage.getItem('alreadyLanuch').then(value => {
+      if (value == null) {
+        AsyncStorage.setItem('alreadyLunched', 'true');
+        setIsFirstLaunch(true);
+      } else {
+        setIsFirstLaunch(false);
+      }
+    });
+  }, []);
+
+  if (isFirstLaunch === null) {
+    return null;
+  } else if (isFirstLaunch == true) {
+    routeName = 'Onboarding';
+  } else {
+    routeName = 'Login';
+  }
+
+  return (
+    <Stack.Navigator initialRouteName={routeName}>
+      <Stack.Screen
+        name="Onboarding"
+        component={OnboardingScreen}
+        options={{header: () => null}}
+      />
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{header: () => null}}
+      />
+      <Stack.Screen name="Signup" component={SignupScreen} />
+      <Stack.Screen name="Trending" component={HomeScreen} />
+      <Stack.Screen name="GetNews" component={GetNews} />
+      <Stack.Screen
+        name="WebView"
+        component={WebViewComponent}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default AuthStack;
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,18 +1,17 @@
 import React from 'react';
 
-import {View, Text, Button, StyleSheet} from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
+
+import FormInput from '../components/FormInput';
 
 const LoginScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
-      
-      <Text>Login Screen</Text>
-      
-      <Button
-        title="Click Here"
-        onPress={() => navigation.navigate('Trending')}
+      <Image
+        source={require('../assets/images/rn-social-logo.png')}
+        style={styles.logo}
       />
+      <Text style={styles.text}>NewsMart News App</Text>
     </View>
   );
 };
@@ -21,8 +20,34 @@ export default LoginScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
+    backgroundColor: '#f9fafd',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    //paddingTop: 50,
+    flex: 1,
+  },
+  logo: {
+    height: 150,
+    width: 150,
+    resizeMode: 'cover',
+  },
+  text: {
+    fontFamily: 'Kufam-SemiBoldItalic',
+    fontSize: 28,
+    marginBottom: 10,
+    color: '#051d5f',
+  },
+  navButton: {
+    marginTop: 15,
+  },
+  forgotButton: {
+    marginVertical: 35,
+  },
+  navButtonText: {
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#2e64e5',
+    fontFamily: 'Lato-Regular',
   },
 });
