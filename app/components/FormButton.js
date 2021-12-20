d82545debcffc7ea7e048021fbce84681b78c8e5
malsha11 <96194@fhss.sjp.ac.lk>
2021-12-20 15:48:39 +0530

    Style FormButton Component

--- app/components/FormButton.js
+++ app/components/FormButton.js
@@ -12,4 +12,21 @@ const FormButton = ({buttonTitle, ...rest}) => {
 
 export default FormButton;
 
-
+const styles = StyleSheet.create({
+  buttonContainer: {
+    marginTop: 10,
+    width: '100%',
+    height: windowHeight / 15,
+    backgroundColor: '#2e64e5',
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 3,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#ffffff',
+    fontFamily: 'Lato-Regular',
+  },
+});
