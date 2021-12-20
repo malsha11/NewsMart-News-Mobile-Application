3db54d391b5bbd1b7b8e6990e7ba7be284a48b00
malsha11 <96194@fhss.sjp.ac.lk>
2021-12-20 12:48:08 +0530

    Add vector Icons Manually

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import {View, Text, Button, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const LoginScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
+      
       <Text>Login Screen</Text>
+      
       <Button
         title="Click Here"
         onPress={() => navigation.navigate('Trending')}
