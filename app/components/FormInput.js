84199f6cae097d07a60e83b2c25ea57a103fa020
malsha11 <96194@fhss.sjp.ac.lk>
2021-12-20 15:49:32 +0530

    Create FormInput Component

---
+++ app/components/FormInput.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {View, TextInput, StyleSheet} from 'react-native';
+import {windowHeight, windowWidth} from '../utils/Dimentions';
+
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+const FormInput = ({labelValue, placeholderText, iconType, ...rest}) => {
+  return (
+    <View style={styles.inputContainer}>
+      <View style={styles.iconStyle}>
+        <AntDesign name={iconType} size={25} color="#666" />
+      </View>
+      <TextInput
+        value={labelValue}
+        style={styles.input}
+        numberOfLines={1}
+        placeholder={placeholderText}
+        placeholderTextColor="#666"
+        {...rest}
+      />
+    </View>
+  );
+};
+
+export default FormInput;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 5,
+    marginBottom: 10,
+    width: '100%',
+    height: windowHeight / 15,
+    borderColor: '#ccc',
+    borderRadius: 3,
+    borderWidth: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  
+});
