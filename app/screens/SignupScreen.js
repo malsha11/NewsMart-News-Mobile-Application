205914acfdef831bc7edb5fe1963eb1bf002d69a
malsha11 <96194@fhss.sjp.ac.lk>
2021-12-20 19:58:26 +0530

    Create Sign Up Screen

--- app/screens/SignupScreen.js
+++ app/screens/SignupScreen.js
@@ -1,15 +1,75 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
 
-import {View, Text, Button, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  Platform,
+  StyleSheet,
+} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FormButton from '../components/FormButton';
+import FormInput from '../components/FormInput';
+import SocialButton from '../components/SocialButton';
 
 const SignupScreen = ({navigation}) => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
   return (
     <View style={styles.container}>
-      <Text>Signup Screen</Text>
-      <Button
-        title="Click Here"
-        onPress={() => navigation.navigate('Trending')}
+      <Text style={styles.text}>Create an account</Text>
+      <FormInput
+        labelValue={email}
+        onChangeText={userEmail => setEmail(userEmail)}
+        placeholderText="Email"
+        iconType="user"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        autoCorrect={false}
       />
+
+      <FormInput
+        labelValue={password}
+        onChangeText={userPassword => setPassword(userPassword)}
+        placeholderText="Password"
+        iconType="lock"
+        secureTextEntry={true}
+      />
+
+      <FormButton
+        buttonTitle="Sign Up"
+        onPress={() => alert('Sign Up Clicked!')}
+      />
+
+      <TouchableOpacity style={styles.forgotButton} onPress={() => {}}>
+        <Text style={styles.navButtonText}>Forgot Password?</Text>
+      </TouchableOpacity>
+
+      <SocialButton
+        buttonTitle="Sign In with Facebook"
+        btnType="facebook"
+        color="#4867aa"
+        backgroundColor="#e6eaf4"
+        onPress={() => {}}
+      />
+
+      <SocialButton
+        buttonTitle="Sign In with Google"
+        btnType="google"
+        color="#de4d41"
+        backgroundColor="#f5e7ea"
+        onPress={() => {}}
+      />
+
+      <TouchableOpacity
+        style={styles.forgotButton}
+        onPress={() => navigation.navigate('Signup')}>
+        <Text style={styles.navButtonText}>
+          Don't have and account ? Create here{' '}
+        </Text>
+      </TouchableOpacity>
+
     </View>
   );
 };
@@ -18,8 +78,11 @@ export default SignupScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
+    backgroundColor: '#f9fafd',
     justifyContent: 'center',
-  },
+    alignItems: 'center',
+    padding: 20,
+    //paddingTop: 50,
+    flex: 1,
+  
 });
